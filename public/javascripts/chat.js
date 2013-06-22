d64e8bcc343abd125a03918c724dad53801a7d04
Matias Linares <matiaslina@gmail.com>
2013-06-22 12:18:49 -0300

    Agregado sockett.io

---
+++ public/javascripts/chat.js
@@ -0,0 +1,27 @@
+window.onload = function () {
+    var messages = [];
+    var socket = io.connect('http://localhost:3000');
+    var field = document.getElementById("field");
+    var sendButton = document.getElementById("send");
+    var content = document.getElementById("content");
+    var name = document.getElementById ('name');
+ 
+    socket.on('message', function (data) {
+        if(data.message) {
+            messages.push(data.author + ": " + data.message);
+            var html = '';
+            for(var i=0; i<messages.length; i++) {
+                html += messages[i] + '<br />';
+            }
+            content.innerHTML = html;
+        } else {
+            console.log("There is a problem:", data);
+        }
+    });
+ 
+    sendButton.onclick = function() {
+        var text = field.value;
+        var author_v = name.value;
+        socket.emit('send', { message: text, author: author_v });
+    };
+}
