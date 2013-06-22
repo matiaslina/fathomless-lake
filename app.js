44f572741ead5c0eaba50e348e2755f9ee199cf6
Matias Linares <matiaslina@gmail.com>
2013-06-22 12:56:13 -0300

    safjo

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ var server = http.createServer(app).listen(app.get('port'), function(){
     console.log('Express server listening on port ' + app.get('port'));
 });
 
-var io = require("socket.io").listen(app);
+var io = require("socket.io").listen(server);
 
 // assuming io is the Socket.IO server object
 io.configure(function () { 
