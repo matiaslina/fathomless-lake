858ff2ccf3bfa1a9485c0674cde96f4d32b6ea39
Matias Linares <matiaslina@gmail.com>
2013-07-11 18:04:39 -0300

    Casi hecho el compilador, creo que lo mejor es hacerlo aca y no en una plataforma distinta

--- libs/compiler.js
+++ libs/compiler.js
@@ -6,7 +6,12 @@
  *        and compare the stdout to the test @Test 
  */
 
-var exec = require ('child_process').exec;
+var exec            = require ('child_process').exec,
+    events          = require ('events'),
+    event_emitter   = new events.EventEmitter(),
+    Firebase        = require('firebase'),
+    firebase_root   = new Firebase('https://fathomless-lake.firebaseio.com/'),
+    fb_tester       = firebase_root.child('tester');
 
 /*
  * exec_command runs a system command.
@@ -19,9 +24,17 @@ var exec = require ('child_process').exec;
  *          //somethings
  *      }
  */
-function exec_command (command, callback) {
-    var cmd = exec (command, callback);
+function exec_command (command,test) {
+    var cmd = exec (command);
     console.log ("[" + cmd.pid + "] Running child process " + command);
+    cmd.stdout.on ('data', function (data) {
+        event_emitter.emit ('stdout', data, test);
+    });
+
+    cmd.on ('exit', function (code, signal) {
+        console.log("[" + cmd.pid + "] Program terminated with code " + code +
+                    " And signal " + signal );
+    });
 };
 
 function compiler (source) {
@@ -49,49 +62,76 @@ function compiler (source) {
 };
 
 function Test () {
-    this.count = 0;
     this.tests = [];
     this.executable = undefined;
     this.passed_test = [];
-    this.unpassed_test = [];
-    this.add = function (_input, _output) {
+    this.finished = false;
+    this.__proto__ = events.EventEmitter.prototype;
+
+    this.store = function () {
+        if (this.finished) {
+            fb_tester.set ({
+                count: this.tests.length,
+                executable: this.executable,
+                test: this.tests,
+                passed_test: this.passed_test
+            });
+        }
+    };
+
+    this.add = function (_n, _input, _output) {
         this.tests.push ({
+            number: _n,
             input: _input,
             output: _output
         });
-        this.count += 1;
+        this.passed_test.push(false);
     };
     this.clean_all = function () {
         this.tests = [];
-        this.count = 0;
         this.executable = undefined;
         this.passed_test = [];
-        this.unpassed_test = [];
     };
 
     this.clean_io = function () {
         this.passed_test = [];
-        this.unpassed_test = [];
+    };
+    
+    var exec_command = function (command,test) {
+        var cmd = exec (command);
+        console.log ("[" + cmd.pid + "] Running child process " + command);
+        cmd.stdout.on ('data', function (data) {
+            this.emit ('stdout', data, test);
+        });
+
+        cmd.on ('exit', function (code, signal) {
+            console.log("[" + cmd.pid + "] Program terminated with code " + code +
+                        " And signal " + signal );
+        });
     };
 
     this.run = function () {
         for (i=0; i < this.tests.length; i++) {
             console.log ("Running test n " + i);
             var command = 'echo "'+this.tests[i].input+'" | ./'+this.executable;
-            exec_command (command, function (err, stdout, stderr) {
-                if (err != null) {
-                    // some error
-                    console.log ("ERROR -> " + err);
-                    return;
-                }
-                if (stdout == this.tests[i].output) {
-                    this.passed_test.push (i);
-                } else {
-                    this.unpassed_test.push (i);
-                }
-            });
+            exec_command (command, this.tests[i]);
         }
+
+        var timeout = true;
+        for (var i = 0; i < 10 ; i++) {
+            setTimeout (this.store, 1000);
+        };
+        if (timeout)
+            console.log ("[Timeout Error] Cannot store the data in firebase,"+
+                         "The run function haven't finish");
     };
+
+    this._on_stdout_cb = function (data, test) {
+        console.log(this);
+        this.passed_test[test.number] = (data == test.output);
+        this.finished = (this.passed_test.length == this.tests.length);
+    };
+    this.on ('stdout', this._on_stdout_cb);
 };
 
 /* Exports */
@@ -111,12 +151,11 @@ setTimeout (function () {
     /* Set the executable */
     test.executable = "myex";
     /* Add a test */
-    test.add ('100000000','NO');
+    test.add (0,'2','3');
     /* Run the test */
     test.run();
     /* Wait a little */
     setTimeout ((function () {
         console.log ("passed test: " + test.passed_test);
-        console.log ("unpassed test: " + test.unpassed_test);
     }), 5000);
 },2000);
